161475d6989980dcb97d562232572e73c68dffdc
tobka <tobias.lindstrom@protonmail.com>
2022-08-22 16:58:27 +0200

    generate latest plots

--- generate_plots.py
+++ generate_plots.py
@@ -1,8 +1,9 @@
 
-from health_necessary_time_code import minwage_barplot
+from health_necessary_time_code import minwage_barplot, minwage_scatterplot
 
 def main():
     minwage_barplot.plot(False)
+    minwage_scatterplot.plot(False)
 
 if __name__ == '__main__':
     main()
