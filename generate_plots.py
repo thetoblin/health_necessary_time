0e168425ddd92422107d14c557c5c67883626fe0
tobka <tobias.lindstrom@protonmail.com>
2022-08-29 21:53:20 +0200

    add living-wage estimate for the USA

--- generate_plots.py
+++ generate_plots.py
@@ -1,9 +1,10 @@
 
-from health_necessary_time_code import minwage_barplot, minwage_scatterplot
+from health_necessary_time_code import minwage_barplot, minwage_scatterplot, livwage_barplot
 
 def main():
     minwage_barplot.plot(False)
     minwage_scatterplot.plot(False)
+    livwage_barplot.plot(False)
 
 if __name__ == '__main__':
     main()
